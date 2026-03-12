ab580ed221b2b89e1b4b2038306dbd198cf82fb9
alexr <alexross1978@googlemail.com>
2026-03-12 15:01:51 +0400

    Add chained test suite and TMDB IMDB

--- src/test/java/PlexServerTest.java
+++ src/test/java/PlexServerTest.java
@@ -1,16 +1,21 @@
+import io.restassured.path.xml.XmlPath;
 import org.testng.annotations.Test;
 
-import java.util.Properties;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class PlexServerTest extends BaseTest {
 
+    private String ratingKey;
+    private String tmdbId;
+    private String imdbId;
+
     @Test
     public void testServerIdentity() {
         given()
-                .header("X-Plex-Token", config.getProperty("token"))
+                .header("X-Plex-Token", token)
                 .when()
                 .get("/identity")
                 .then()
@@ -21,7 +26,7 @@ public class PlexServerTest extends BaseTest {
     @Test
     public void testGetLibraries() {
         given()
-                .header("X-Plex-Token", config.getProperty("token"))
+                .header("X-Plex-Token", token)
                 .when()
                 .get("/library/sections")
                 .then()
@@ -32,20 +37,26 @@ public class PlexServerTest extends BaseTest {
 
     @Test
     public void testGetRecentlyAdded() {
-        given()
-                .header("X-Plex-Token", config.getProperty("token"))
-                .when()
+        String response = given()
+                .header("X-Plex-Token", token)
+            .when()
                 .get("/library/recentlyAdded?X-Plex-Container-Size=100&type=1")
-                .then()
+            .then()
                 .statusCode(200)
                 .body(containsString("MediaContainer"))
-                .body(containsString("type=\"movie\""));
+                .body(containsString("type=\"movie\""))
+                .extract().body().asString();
+
+        XmlPath xmlPath = new XmlPath(response);
+        ratingKey = xmlPath.getString("MediaContainer.Video[0].@ratingKey");
+
+        System.out.println("Rating key extracted: " + ratingKey);
     }
 
     @Test
     public void testItemMetadata() {
         given()
-                .header("X-Plex-Token", config.getProperty("token"))
+                .header("X-Plex-Token", token)
                 .when()
                 .get("/library/metadata/8659")
                 .then()
@@ -53,4 +64,45 @@ public class PlexServerTest extends BaseTest {
                 .body(containsString("imdb://"))
                 .body(containsString("tmdb://"));
     }
+
+    @Test(dependsOnMethods = "testGetRecentlyAdded")
+    public void testGetItemMetadata() {
+        String response = given()
+                .header("X-Plex-Token", token)
+                .when()
+                .get("/library/metadata/" + ratingKey)
+                .then()
+                .statusCode(200)
+                .body(containsString("imdb://"))
+                .body(containsString("tmdb://"))
+                .extract().body().asString();
+        System.out.println(TestUtils.prettyPrintXml(response));
+
+        XmlPath xmlPath = new XmlPath(response);
+
+        List<String> guids = xmlPath.getList("MediaContainer.Video.Guid.@id");
+        for (String guid : guids) {
+            if (guid.startsWith("tmdb://")) {
+                tmdbId = guid.replace("tmdb://", "");
+            }
+            if (guid.startsWith("imdb://")) {
+                imdbId = guid.replace("imdb://", "");
+            }
+        }
+        System.out.println("TMDB ID extracted: " + tmdbId);
+        System.out.println("TMDB ID extracted: " + imdbId);
+    }
+
+    @Test(dependsOnMethods = "testGetItemMetadata")
+    public void testTmdbLookup() {
+        given()
+                .queryParam("api_key", config.getProperty("tmdb.apiKey"))
+                .when()
+                .get(config.getProperty("tmdb.baseUrl") + "/movie/" + tmdbId)
+                .then()
+                .statusCode(200)
+                .body(containsString("imdb_id"))
+                .body(containsString("poster_path"));
+    }
+
 }
